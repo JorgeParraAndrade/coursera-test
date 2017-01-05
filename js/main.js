f9879998eab15db89c2042a4c7905faa787366e1
DESKTOP-1F6FBTI\Jorge <jorgeparraandrade@gmail.com>
2017-01-05 19:22:43 -0400

    no message

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 
 //smooth scroll
-$('a[href*="#"]:not([href="#"])').not(".carousel-control").click(function() {
+$('a[href*="#"]:not([href="#"])').not(".carousel-control,.no-smooth").click(function() {
   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
